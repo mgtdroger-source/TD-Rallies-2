4f1fcd266719179e2ae66d4f0eae567b58a6f448
mgtdroger-source <mgtdroger@gmail.com>
2026-05-07 18:28:11 +0200

    Delete Launch_TD_Rallies_2.cmd

--- Launch_TD_Rallies_2.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "BASE=%~dp0"
-set "INDEX=%BASE%index.html"
-set "PROFILE=%BASE%.profile-edge-2"
-set "URL=file:///%INDEX:\=/%"
-
-start "" msedge --app="%URL%" --user-data-dir="%PROFILE%" --allow-file-access-from-files --window-size=1600,980 --window-position=40,40
